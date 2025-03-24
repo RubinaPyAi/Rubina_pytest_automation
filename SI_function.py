c69d3c86cbe8fc0d16f45a8ff6bd3b6242c430e3
RubinaPyAi <rubinahanfi24@gmail.com>
2025-03-24 09:19:56 +0530

    new file involving the calculation of Simple interest is added

---
+++ SI_function.py
@@ -0,0 +1,21 @@
+
+def interest(p,t=2,r=0.10):
+    s=(p*r*t)/100
+    return s
+
+prin=int(input("enter the principal: "))
+simpleInterest=interest(prin)
+print("simple interest with default ROI and time value is:",simpleInterest)
+time=float(input("enter the time:"))
+rate=float(input("enter the rate: "))
+SI=interest(prin,time,rate)
+print("The calculated simple interest with your provided ROI and time values is=",SI)
+
+
+
+
+
+
+
+   
+
